27d928b4797993609f8e9ea7276958103d7193c8
dangdaizheshi <2022702538@qq.com>
2025-12-11 20:59:04 +0800

    login

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,11 +1,55 @@
 <template>
-  <div>sg</div>
+  <div class="container">
+    <el-row>
+      <el-col :span="12" :xs="0">左侧区域</el-col>
+      <el-col :span="12" :xs="24">
+          <el-form class="login_form">
+            <h1>Hello</h1>
+            <h2>后台管理系统</h2>
+            <el-form-item label="用户名">
+              <el-input :prefix-icon="User"></el-input>
+            </el-form-item>
+            <el-form-item label="用户名">
+              <el-input type="password"></el-input>
+            </el-form-item>
+             <el-form-item class="login_btn">
+              <el-button type="primary" :prefix-icon="Lock">登录</el-button>
+            </el-form-item>
+          </el-form>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
-<script setup>
-
+<script setup lang="ts">
+import { Calendar, Search, User, Lock } from '@element-plus/icons-vue'
 </script>
 
 <style lang="scss" scoped>
-
+.container {
+  width: 100%;
+  height: 100vh;
+  background: url('../../assets/images/baokemeng.png') no-repeat center;
+  background-size: cover;
+  .login_form {
+    padding: 40px;
+    position: relative;
+    width: 75%;
+    top: 30vh;
+    background: url('../../assets/images/login_form.png') no-repeat;
+    background-size: cover;
+    h1 {
+      color: white;
+      font-size: 40px;
+    }
+    h2 {
+      color: white;
+      font-size: 20px;
+      margin: 20px 0;
+    }
+    .login_btn {
+      width: 100%;
+    }
+  }
+}
 </style>
