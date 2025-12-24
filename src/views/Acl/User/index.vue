0ffa5e73a022a21602dde64444e9fbaebfa176b7
dangdaizheshi <2022702538@qq.com>
2025-12-24 14:14:10 +0800

    完成右侧复选框

--- src/views/Acl/User/index.vue
+++ src/views/Acl/User/index.vue
@@ -9,9 +9,9 @@
           <el-input></el-input>
         </el-form-item>
         <el-form-item label="职位列表">
-          <el-checkbox>全选</el-checkbox>
-          <el-checkbox-group>
-            <el-checkbox v-for="city in 10">
+          <el-checkbox v-model="checkedAll" :indeterminate="indeterminate" @change="handleCheckAllChange">全选</el-checkbox>
+          <el-checkbox-group v-model="checkedCities" @change="handleCheckedCitiesChange">
+            <el-checkbox v-for="(city, index) in cities" :label="city" :key="index">
               {{ city }}
             </el-checkbox>
           </el-checkbox-group>
@@ -30,7 +30,20 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 let drawer1 = ref(true)
+let checkedAll = ref(false)
+let indeterminate = ref(false)
+const cities = ['Shanghai', 'Beijing', 'Guangzhou', 'Shenzhen', 'Hangzhou', 'Wuhan', 'Nanjing', 'Chongqing', 'Tianjin', 'Kunming']
+const checkedCities = ref(['Shanghai', 'Beijing'])
 
+const handleCheckAllChange = (val: boolean) => {
+  val ? (checkedCities.value = cities) : (checkedCities.value = [])
+  indeterminate.value = false
+}
+const handleCheckedCitiesChange = (value: string[]) => {
+  const checkedCount = value.length
+  checkedAll.value = checkedCount === cities.length
+  indeterminate.value = !(checkedCount > 0 && checkedCount < cities.length)
+}
 </script>
 
 <style lang="scss" scoped>
